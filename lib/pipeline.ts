6762fe6ee4dd88c5c32e28123f5c6bfb4ea0918d
EC2 Default User <ec2-user@ip-172-31-44-179.ap-northeast-2.compute.internal>
2022-08-16 07:19:20 +0000

    Bootstrapping ArgoCD

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -10,10 +10,12 @@ export default class PipelineConstruct extends Construct {
     const account = props?.env?.account!;
     const region = props?.env?.region!;
 
+    const addOn = new blueprints.addons.ArgoCDAddOn();
+
     const blueprint = blueprints.EksBlueprint.builder()
     .account(account)
     .region(region)
-    .addOns()
+    .addOns(addOn)
     .teams();
   
     blueprints.CodePipelineStack.builder()
@@ -24,10 +26,9 @@ export default class PipelineConstruct extends Construct {
           credentialsSecretName: 'github-token',
           targetRevision: 'main'
       })
-      // 작성하신 pipeline.ts 코드의 이 부분 뒤에 아래 코드(.wave)를 추가해주세요!
       .wave({
         id: "envs",
-        stages: [   
+        stages: [
           { id: "dev", stackBuilder: blueprint.clone('us-east-1')},
           { id: "prod", stackBuilder: blueprint.clone('ap-northeast-2')}
         ]
